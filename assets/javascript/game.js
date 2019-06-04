8e396ecf443a56bac61b490a44935dd4a4febe20
Phillip Valdez <Phillip0150@gmail.com>
2019-06-04 15:51:32 -0500

    updated index and game.js
    added a object to game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,3 +8,18 @@
 // If the player chooses the wrong answer, tell the player they selected the wrong option and then display the correct answer. Wait a few seconds, then show the next question.
 // On the final screen, show the number of correct answers, incorrect answers, and an option to restart the game (without reloading the page).
 
+var gameData = {
+    questions : [],
+    answers: [[],[]],
+    correctAnswers: [],
+    userCorrectAnswers: [],
+    userIncorrectAnswers: [],
+    userNoAnswers: [],
+
+    restart: function() {
+        this.userCorrectAnswers = [];
+        this.userIncorrectAnswer = [];
+        this.userNoAnswers = [];
+    }
+};
+
