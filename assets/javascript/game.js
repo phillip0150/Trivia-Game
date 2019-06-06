7ea8b564625125b601eed41e3c477c8fc7ae1f8b
Phillip Valdez <Phillip0150@gmail.com>
2019-06-05 20:25:49 -0500

    updated game.js, index.html, game(saveforlater).js
    Was able to make the program functional. Need to go back and create style to the page

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,75 +1,280 @@
-// You'll create a trivia game that shows only one question until the player answers it or their time runs out.
+var question = ["During the early stages of production, WHO auditioned for the role of Jerry.",
+"Who does the voice Jerry?",
+"A poster of WHAT RAPPER(S) can be seen in Summer's room on the door",
+"Who does the voice of Beth?",
+"When does season 3 come out?"];
+var questionAnswers= {
+    a0: ['Bryan Cranston', 'The Rock', 'Kevin Hart', 'Brad Pitt'],
+    a1: ['Justin Bieber', 'Chris Patt', 'Chris Parnell', 'Bryan Cranston'],
+    a2: ["Tupac Shakur", "Lil Uzi Vert", "Wu-Tang Clan", "Ice Cube"],
+    a3: ["Taylor Swift", "Sarah Chalke", "Naomi Scott", "Emilia Clarke"],
+    a4: ["12-12-19", "08-18-19", "07-19-19", "It's already out"]
+};
+var answers = ["Bryan Cranston","Chris Parnell", "Tupac Shakur", "Sarah Chalke", "It's already out"];
 
-// If the player selects the correct answer, show a screen congratulating them for choosing the right option. After a few seconds, display the next question -- do this without user input.
+var displayCorrectId;
+var timerId;
+var textCorrectAnswer;
+var count = 0;
+var seconds = 31;
+var answerSeconds = 4;
+var right = 0;
+var wrong = 0;
+var noAnswer = 0;
 
-// The scenario is similar for wrong answers and time-outs.
 
-// If the player runs out of time, tell the player that time's up and display the correct answer. Wait a few seconds, then show the next question.
-// If the player chooses the wrong answer, tell the player they selected the wrong option and then display the correct answer. Wait a few seconds, then show the next question.
-// On the final screen, show the number of correct answers, incorrect answers, and an option to restart the game (without reloading the page).
-
-var gameData = {
-    questions : ["During the early stages of production, WHO auditioned for the role of Jerry.",
-        "Who does the voice Jerry?",
-        "A poster of WHAT RAPPER(S) can be seen in Summer's room on the door",
-        "Who does the voice of Beth?",
-        "When does season 3 come out?"],
-    answers: {
-        a0: ['Bryan Cranston', 'The Rock', 'Kevin Hart', 'Brad Pitt'],
-        a1: ['Justin Bieber', 'Chris Patt', 'Chris Parnell', 'Bryan Cranston'],
-        a2: ["Tupac Shakur", "Lil Uzi Vert", "Wu-Tang Clan", "Ice Cube"],
-        a3: ["Taylor Swift", "Sarah Chalke", "Naomi Scott", "Emilia Clarke"],
-        a4: ["12-12-19", "08-18-19", "07-19-19", "It's already out"]
-    },
-    correctAnswers: ["Bryan Cranston","Chris Parnell", "Tupac Shakur", "Sarah Chalke", "It's already out"],
-    userCorrectAnswers: [],
-    userIncorrectAnswers: [],
-    userNoAnswers: [],
-    questionCount: 0,
-
-    restart: function() {
-        this.userCorrectAnswers = [];
-        this.userIncorrectAnswer = [];
-        this.userNoAnswers = [];
+function displayQuestion() {
+    if (count ===0){
+        $("#question").html("<p>"+question[count]+"</p><br>"); 
+        $("#answer0").html("<button>"+questionAnswers.a0[0]+"</button><br>");
+        $("#answer1").html("<button>"+questionAnswers.a0[1]+"</button><br>");
+        $("#answer2").html("<button>"+questionAnswers.a0[2]+"</button><br>");
+        $("#answer3").html("<button>"+questionAnswers.a0[3]+"</button><br>");
     }
-};
+    if (count ===1){
+        $("#question").html("<p>"+question[count]+"</p><br>"); 
+        $("#answer0").html("<button>"+questionAnswers.a1[0]+"</button><br>");
+        $("#answer1").html("<button>"+questionAnswers.a1[1]+"</button><br>");
+        $("#answer2").html("<button>"+questionAnswers.a1[2]+"</button><br>");
+        $("#answer3").html("<button>"+questionAnswers.a1[3]+"</button><br>");
+    }
+    if (count ===2){
+        $("#question").html("<p>"+question[count]+"</p><br>"); 
+        $("#answer0").html("<button>"+questionAnswers.a2[0]+"</button><br>");
+        $("#answer1").html("<button>"+questionAnswers.a2[1]+"</button><br>");
+        $("#answer2").html("<button>"+questionAnswers.a2[2]+"</button><br>");
+        $("#answer3").html("<button>"+questionAnswers.a2[3]+"</button><br>");
+    }
+    if (count ===3){
+        $("#question").html("<p>"+question[count]+"</p><br>"); 
+        $("#answer0").html("<button>"+questionAnswers.a3[0]+"</button><br>");
+        $("#answer1").html("<button>"+questionAnswers.a3[1]+"</button><br>");
+        $("#answer2").html("<button>"+questionAnswers.a3[2]+"</button><br>");
+        $("#answer3").html("<button>"+questionAnswers.a3[3]+"</button><br>");
+    }
+    if (count ===4){
+        $("#question").html("<p>"+question[count]+"</p><br>"); 
+        $("#answer0").html("<button>"+questionAnswers.a4[0]+"</button><br>");
+        $("#answer1").html("<button>"+questionAnswers.a4[1]+"</button><br>");
+        $("#answer2").html("<button>"+questionAnswers.a4[2]+"</button><br>");
+        $("#answer3").html("<button>"+questionAnswers.a4[3]+"</button><br>");
+    }
+    if (count > 4) {
+        endGame();
+    }
+ 
+}
 
-function displayQuestions(x){
-    $("#question").text(gameData.questions[x]);
+var running = false;
+var textAnswer = false;
 
-};
+function runTimer() {
+    if (!running){
+    timerId = setInterval(decrementTime, 1000);
+    running = true;
+    }
+}
 
-function displayAnswers(answerObject){
-    $("#answer0").html("<button>"+answerObject[0]+"</button><br>");
-    $("#answer1").html("<button>"+answerObject[1]+"</button><br>");
-    $("#answer2").html("<button>"+answerObject[2]+"</button><br>");
-    $("#answer3").html("<button>"+answerObject[3]+"</button><br>");
+function decrementTime() {
+
+    seconds--;
+
+    $("#time").html("<h2>" + seconds + "</h2>");
+
+    if (seconds <= 0) {
+
+    stopTimer();
+    runDisplayCorrectAnswer("YOUR WRONG", "CORRECT ANSWER IS " +answers[count]);
+    noAnswer++;
+    count++;
     
-};
 
-//function to display the correct answer
-//here the user will know if they got the answer right or wrong (update text)
-function displayCorrectAnswer(rightWrong, theCorrectAnswer){
-    $("#rightwrong").text(rightWrong);
-    $("#theCorrectAnswer").text(theCorrectAnswer);
     
-};
+    }
+    
+}
 
-$(document).ready(function() {
+function stopTimer() {
+    clearInterval(timerId);
+    running = false;
+    seconds = 31;
+}
 
-    $("#theButton").click(function(){
-        console.log("in click");
+function startGame() {
+    $("#tryAgain").hide();
+    if (count > 4) {   
+        endGame();
+    }
+    else {
+        runTimer();
+        displayQuestion();
         $("#theGame").show();
+        $("#question").show();
+        $("#answers").show();
+        $("#time").show();
+        $("#showAnswer").hide();
         $("#theButton").hide();
-        displayQuestions(0);
-        displayAnswers(gameData.answers.a0);
 
-    });
+    }
 
-    $("#answer0").click(function(){
+}
 
-    });
-
+function runDisplayCorrectAnswer(rightWrong, theCorrectAnswer){
+    if(!textAnswer){
+        displayCorrectId = setInterval(function(){
+            answerSeconds--;
+            $("#rightwrong").text(rightWrong);
+            $("#theCorrectAnswer").text(theCorrectAnswer);
+            $("#question").hide();
+            $("#answers").hide();
+            $("#time").hide();
+            $("#showAnswer").show();
+            console.log("In display Correct Answer function");
+            if (answerSeconds <= 0){
+                stopDisplayCorrectAnswer();
+                
+                startGame();  
+            }
+        }, 2000);
+        textAnswer = true;    
+    }
     
+    
+};
+
+function stopDisplayCorrectAnswer() {
+    clearInterval(displayCorrectId);
+    textAnswer= false;
+    answerSeconds =4;
+    console.log("textAnswer" + textAnswer);
+    
+
+}
+
+function endGame() {
+    $("#showAnswer").empty();
+    $("#question").hide();
+    $("#answers").hide();
+    $("#time").hide();
+    $("#tryAgain").show();
+    $("showAnswer").show();
+    $("#showAnswer").text("You answered " + right + " right, " + wrong + " wrong, and "+noAnswer+ " unanswered");
+    $("#tryAgain").html("<button>TRY AGAIN?</button>");
+}
+
+$("#theButton").click(startGame);
+
+
+$("#answer0").click(function() {
+    stopTimer();
+    var userPick = $("#answer0").text();
+    if(userPick === answers[count]) {
+        console.log("right");
+        runDisplayCorrectAnswer("YOUR RIGHT", "CORRECT ANSWER IS " +answers[count]);
+        right++; 
+        
+
+    }
+    else {
+        console.log("wrong");
+        runDisplayCorrectAnswer("YOUR WRONG", "CORRECT ANSWER IS " +answers[count]);
+        wrong++;
+ 
+    } 
+    count++;
+
+});
+
+$("#answer1").click(function() {
+    stopTimer();
+    var userPick = $("#answer1").text();
+    if(userPick === answers[count]) {
+        console.log("right");
+        runDisplayCorrectAnswer("YOUR RIGHT", "CORRECT ANSWER IS " +answers[count]);
+        right++; 
+        
+
+    }
+    else {
+        console.log("wrong");
+        runDisplayCorrectAnswer("YOUR WRONG", "CORRECT ANSWER IS " +answers[count]);
+        wrong++;
+ 
+    } 
+    count++;
+
+});
+
+$("#answer2").click(function() {
+    stopTimer();
+    var userPick = $("#answer2").text();
+    if(userPick === answers[count]) {
+        console.log("right");
+        runDisplayCorrectAnswer("YOUR RIGHT", "CORRECT ANSWER IS " +answers[count]);
+        right++; 
+        
+
+    }
+    else {
+        console.log("wrong");
+        runDisplayCorrectAnswer("YOUR WRONG", "CORRECT ANSWER IS " +answers[count]);
+        wrong++;
+ 
+    } 
+    count++;
+
+});
+
+$("#answer3").click(function() {
+    stopTimer();
+    var userPick = $("#answer3").text();
+    if(userPick === answers[count]) {
+        console.log("right");
+        runDisplayCorrectAnswer("YOUR RIGHT", "CORRECT ANSWER IS " +answers[count]);
+        right++; 
+        
+
+    }
+    else {
+        console.log("wrong");
+        runDisplayCorrectAnswer("YOUR WRONG", "CORRECT ANSWER IS " +answers[count]);
+        wrong++;
+ 
+    } 
+    count++;
+
+});
+
+$("#answer4").click(function() {
+    stopTimer();
+    var userPick = $("#answer4").text();
+    if(userPick === answers[count]) {
+        console.log("right");
+        runDisplayCorrectAnswer("YOUR RIGHT", "CORRECT ANSWER IS " +answers[count]);
+        right++; 
+        
+
+    }
+    else {
+        console.log("wrong");
+        runDisplayCorrectAnswer("YOUR WRONG", "CORRECT ANSWER IS " +answers[count]);
+        wrong++;
+ 
+    } 
+    count++;
+
+});
+
+$("#tryAgain").click(function(){
+    displayCorrectId;
+    timerId;
+    textCorrectAnswer;
+    count = 0;
+    seconds = 31;
+    answerSeconds = 4;
+    right = 0;
+    wrong = 0;
+    noAnswer = 0;
+    startGame();
 });
 
